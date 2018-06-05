a8ffa086e8394d6c21339fd3791989ec20cb7494
Arsham Shirvani <arshamshirvani@gmail.com>
2018-06-05 13:46:59 +0100

    remove commented lines from main.go

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
-import (
-	"github.com/arsham/blush/cmd"
-)
+import "github.com/arsham/blush/cmd"
 
 func main() {
-	// defer profile.Start(profile.MemProfile, profile.CPUProfile).Stop()
-	// defer profile.Start( profile.TraceProfile).Stop()
 	cmd.Main()
 }
